ec01a3dfe22cf569118764c6e762332334b0d375
Mathieu <mat_lefebvre12@hotmail.com>
2022-06-27 21:49:23 -0400

    l

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,30 +3,36 @@ import React from "react";
 import Header from "../components/Header";
 import { Link } from "react-router-dom";
 import Subscibe from "../components/Subscibe";
+import { Card } from "@mui/material";
 export default function Home() {
-
   return (
-    <div>
+    <div className="h-screen">
       <Header></Header>
-      <div className="flex justify-center items-center flex-col w-full h-screen">
-      <div className="flex flex-col justify-center items-center w-full h-1/2">
-       <span className="text-7xl text-black font-Cairo text-center mb-2">Only you can access your passwords.</span>
-       <span className="text-4xl text-black font-Cairo text-center my-5"> First Decentralize Password Manager.</span>
-       <Link to="/Dashboard" className="no-underline flex items-center justify-center h-auto">
-          <Button
-            size="medium"
-            variant="contained"
-            className="bg-cyan-400 hover:bg-blue-500 m-2  font-Neptune hover:transform hover:scale-105"
+      <Card className="flex justify-center items-center flex-col w-full h-1/2 bg-neutral-900">
+        <div className="flex flex-col justify-center items-center w-3/4 h-full">
+          <span className="text-7xl text-white font-Cairo text-center mb-2">
+            Only you can access your passwords.
+          </span>
+          <span className="text-4xl text-black font-Cairo text-center my-5">
+            {" "}
+            First Decentralize Password Manager.
+          </span>
+          <Link
+            to="/Dashboard"
+            className="no-underline flex items-center justify-center h-auto"
           >
-            Launch App
-          </Button>
-        </Link>
-      </div>
-      </div>
-     
-      
+            <Button
+              size="medium"
+              variant="contained"
+              className="bg-cyan-400 hover:bg-blue-500 my-16  font-Neptune hover:transform hover:scale-105"
+            >
+              Launch App
+            </Button>
+          </Link>
+        </div>
+      </Card>
 
-        <Subscibe></Subscibe>
+      <Subscibe></Subscibe>
     </div>
   );
 }
